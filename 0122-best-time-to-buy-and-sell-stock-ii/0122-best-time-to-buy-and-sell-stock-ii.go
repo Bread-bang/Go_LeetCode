43ab474717258f8b95cdb29b1f57f16407c8c417
Bread-bang <65281350+Bread-bang@users.noreply.github.com>
2026-01-07 09:49:45 +0900

    Time: 0 ms (100%), Space: 5.1 MB (80.43%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.go
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.go
@@ -1,16 +1,11 @@
 func maxProfit(prices []int) int {
     l := len(prices)
-	diff := make([]int, l)
 
 	sum := 0
-	diff[0] = 0
 	for i := 1; i < l; i++ {
-		diff[i] = prices[i] - prices[i-1]
-		if diff[i] < 0 {
-			diff[i] = 0
-		}
-
-		sum += diff[i]
+        if prices[i] > prices[i-1] {
+            sum += prices[i] - prices[i-1]
+        }
 	}
 
 	return sum
