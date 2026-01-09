a083104e9647dc33aa87226bf280e7915fdc5e9d
Bread-bang <65281350+Bread-bang@users.noreply.github.com>
2026-01-09 10:24:54 +0900

    Time: 0 ms (100%), Space: 19.4 MB (80.84%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.go
@@ -0,0 +1,15 @@
+func increasingTriplet(nums []int) bool {
+	first, second := math.MaxInt, math.MaxInt
+
+	for _, v := range nums {
+		if v <= first {
+			first = v
+		} else if v <= second {
+			second = v
+		} else {
+			return true
+		}
+	}
+
+	return false
+}
